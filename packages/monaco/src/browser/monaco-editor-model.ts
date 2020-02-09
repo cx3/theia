337f1a2ef1f54b6300766589ed56f902c2602ab1
Anton Kosyakov <anton.kosyakov@typefox.io>
2020-02-09 11:42:08 +0000

    [preferences] fix #6845: use text models to update content
    It resolve following issues:
    - dirty editors are respected
    - edits are applied in thread safe fashion
    
    Signed-off-by: Anton Kosyakov <anton.kosyakov@typefox.io>

--- packages/monaco/src/browser/monaco-editor-model.ts
+++ packages/monaco/src/browser/monaco-editor-model.ts
@@ -75,8 +75,8 @@ export class MonacoEditorModel implements ITextEditorModel, TextEditorDocument {
         this.toDispose.push(this.onDidSaveModelEmitter);
         this.toDispose.push(this.onWillSaveModelEmitter);
         this.toDispose.push(this.onDirtyChangedEmitter);
-        this.resolveModel = resource.readContents(options).then(content => this.initialize(content));
         this.defaultEncoding = options && options.encoding ? options.encoding : undefined;
+        this.resolveModel = this.readContents().then(content => this.initialize(content || ''));
     }
 
     dispose(): void {
@@ -118,6 +118,21 @@ export class MonacoEditorModel implements ITextEditorModel, TextEditorDocument {
                 this.toDispose.push(this.resource.onDidChangeContents(() => this.sync()));
             }
         }
+    }
+
+    /**
+     * Use `valid` to access it.
+     * Use `setValid` to mutate it.
+     */
+    protected _valid = false;
+    /**
+     * Whether it is possible to load content from the underlying resource.
+     */
+    get valid(): boolean {
+        return this._valid;
+    }
+    protected setValid(valid: boolean): void {
+        this._valid = valid;
     }
 
     protected _dirty = false;
@@ -243,9 +258,12 @@ export class MonacoEditorModel implements ITextEditorModel, TextEditorDocument {
             return;
         }
 
-        const newText = await this.readContents();
-        if (newText === undefined || token.isCancellationRequested || this._dirty) {
+        let newText = await this.readContents();
+        if (token.isCancellationRequested || this._dirty) {
             return;
+        }
+        if (newText === undefined) {
+            newText = '';
         }
 
         const value = this.model.getValue();
@@ -261,9 +279,12 @@ export class MonacoEditorModel implements ITextEditorModel, TextEditorDocument {
     }
     protected async readContents(): Promise<string | undefined> {
         try {
-            return await this.resource.readContents({ encoding: this.getEncoding() });
+            const content = await this.resource.readContents({ encoding: this.getEncoding() });
+            this.setValid(true);
+            return content;
         } catch (e) {
             if (ResourceError.NotFound.is(e)) {
+                this.setValid(false);
                 return undefined;
             }
             throw e;
@@ -320,14 +341,15 @@ export class MonacoEditorModel implements ITextEditorModel, TextEditorDocument {
     }
 
     protected fireDidChangeContent(event: monaco.editor.IModelContentChangedEvent): void {
-        const changeContentEvent = this.asContentChangedEvent(event);
-        this.onDidChangeContentEmitter.fire(changeContentEvent);
-        this.pushContentChanges(changeContentEvent.contentChanges);
         if (this.model.getAlternativeVersionId() === this.bufferSavedVersionId) {
             this.setDirty(false);
         } else {
             this.markAsDirty();
         }
+
+        const changeContentEvent = this.asContentChangedEvent(event);
+        this.onDidChangeContentEmitter.fire(changeContentEvent);
+        this.pushContentChanges(changeContentEvent.contentChanges);
     }
     protected asContentChangedEvent(event: monaco.editor.IModelContentChangedEvent): MonacoModelContentChangedEvent {
         const contentChanges = event.changes.map(change => this.asTextDocumentContentChangeEvent(change));
@@ -377,6 +399,7 @@ export class MonacoEditorModel implements ITextEditorModel, TextEditorDocument {
 
         const content = this.model.getValue();
         await Resource.save(this.resource, { changes, content, options: { encoding: this.getEncoding(), overwriteEncoding } }, token);
+        this.setValid(true);
         if (token.isCancellationRequested) {
             return;
         }
